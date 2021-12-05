9344187a03d11c1d750ddace311750f556154056
Jorge <villordo235@gmail.com>
2021-12-05 13:29:37 -0300

    Exception handler created.

---
+++ Backend/src/main/java/com/challenge/api/controllers/handler/ExceptionHandlerController.java
@@ -0,0 +1,32 @@
+package com.challenge.api.controllers.handler;
+
+import com.challenge.api.dto.ErrorDtoResponse;
+import com.challenge.api.exceptions.AlreadyExistsException;
+import com.challenge.api.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ErrorDtoResponse> handleAlreadyExistsException(AlreadyExistsException exception){
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorDtoResponse.builder()
+                .message(exception.getMessage())
+                .exception("AlreadyExistsException")
+                .build());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDtoResponse> handleNotFoundException(NotFoundException exception){
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorDtoResponse.builder()
+                .message(exception.getMessage())
+                .exception("NotFoundException")
+                .build());
+    }
+
+}
