494f5234bca1ab0bdd3a9743b4dfe74f72cd068f
Jorge Villordo <villordo235@gmail.com>
2022-04-12 19:04:52 -0300

    "Ah"

--- Backend/src/main/java/com/challenge/api/controllers/FolderController.java
+++ Backend/src/main/java/com/challenge/api/controllers/FolderController.java
@@ -38,7 +38,7 @@ public class FolderController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-    @PostMapping("/")asd
+    @PostMapping("/")ahre
     public ResponseEntity addFolder(@RequestBody Folder folder) throws AlreadyExistsException {
         return new ResponseEntity<>(folderService.addFolder(folder), HttpStatus.CREATED);
     }
